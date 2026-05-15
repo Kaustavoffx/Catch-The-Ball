a25f169984cdb2109da59c06f9d538617968dfd1
Kaustavoffx <kaustav.chowdhury9999@gmail.com>
2026-05-15 23:41:14 +0530

    Implemented gameplay timer system

--- script.js
+++ script.js
@@ -4,20 +4,35 @@ const scoreText = document.getElementById("score");
 const startBtn = document.getElementById("startBtn");
 const livesText = document.getElementById("lives");
 const pauseBtn = document.getElementById("pauseBtn");
+
 const highScoreText = document.getElementById("highScore");
+
 const gameOverModal = document.getElementById("gameOverModal");
 const finalScoreText = document.getElementById("finalScoreText");
 const restartBtn = document.getElementById("restartBtn");
 
+const timerText = document.getElementById("timer");
+
+let timer = 0;
+
+let timerInterval;
+
 let paused = false;
+
 let basketX = 150;
+
 let ballX = Math.random() * 340;
 let ballY = 0;
+
 let lives = 3;
 let score = 0;
+
 let speed = 4;
+
 let gameInterval;
+
 let gameRunning = false;
+
 let highScore = localStorage.getItem("highScore") || 0;
 
 highScoreText.innerText = highScore;
@@ -27,14 +42,19 @@ document.addEventListener("mousemove", moveBasket);
 function moveBasket(event){
 
     const gameArea = document.getElementById("gameArea");
+
     const rect = gameArea.getBoundingClientRect();
+
     basketX = event.clientX - rect.left - 50;
+
     if(basketX < 0){
         basketX = 0;
     }
+
     if(basketX > 300){
         basketX = 300;
     }
+
     basket.style.left = basketX + "px";
 }
 
@@ -45,6 +65,7 @@ function updateBall(){
     }
 
     ballY += speed;
+
     ball.style.top = ballY + "px";
     ball.style.left = ballX + "px";
 
@@ -56,11 +77,23 @@ function updateBall(){
         ){
 
             score++;
+
             scoreText.innerText = score;
+
+            scoreText.classList.add("scorePop");
+
+            setTimeout(function(){
+
+                scoreText.classList.remove("scorePop");
+
+            }, 200);
+
             if(score > highScore){
 
                 highScore = score;
+
                 localStorage.setItem("highScore", highScore);
+
                 highScoreText.innerText = highScore;
             }
 
@@ -71,18 +104,30 @@ function updateBall(){
         }else{
 
             lives--;
+
             if(lives <= 0){
 
                 lives = 0;
+
                 gameRunning = false;
+
                 clearInterval(gameInterval);
+
+                clearInterval(timerInterval);
+
                 resetBall();
+
                 livesText.innerText = lives;
+
                 finalScoreText.innerText = "Final Score: " + score;
+
                 gameOverModal.style.display = "flex";
+
                 startBtn.disabled = false;
+
                 return;
             }
+
             livesText.innerText = lives;
         }
 
@@ -93,42 +138,65 @@ function updateBall(){
 function resetBall(){
 
     ballY = 0;
+
     ballX = Math.random() * 340;
+
     ball.style.top = ballY + "px";
+
     ball.style.left = ballX + "px";
 }
 
 function startGame(){
+
     gameRunning = true;
+
     paused = false;
+
     pauseBtn.innerText = "Pause";
+
     score = 0;
+
     lives = 3;
+
     speed = 4;
-    
+
+    timer = 0;
+
+    timerText.innerText = timer;
+
     scoreText.innerText = score;
 
     livesText.innerText = lives;
 
     gameOverModal.style.display = "none";
+
     resetBall();
 
     clearInterval(gameInterval);
 
-    scoreText.classList.add("scorePop");
-    setTimeout(function(){
-        scoreText.classList.remove("scorePop");
-    },200);
+    clearInterval(timerInterval);
 
     gameInterval = setInterval(updateBall, 20);
+
+    timerInterval = setInterval(function(){
+
+        timer++;
+
+        timerText.innerText = timer;
+
+    }, 1000);
+
     startBtn.disabled = true;
-    document.getElementById("startScreen").style.display="none";
+
+    document.getElementById("startScreen").style.display = "none";
 }
 
 startBtn.addEventListener("click", startGame);
 
 restartBtn.addEventListener("click", function(){
+
     gameOverModal.style.display = "none";
+
     startGame();
 });
 
@@ -137,15 +205,19 @@ document.addEventListener("keydown", function(event){
     if(event.key === "ArrowLeft"){
         basketX -= 30;
     }
+
     if(event.key === "ArrowRight"){
         basketX += 30;
     }
+
     if(basketX < 0){
         basketX = 0;
     }
+
     if(basketX > 300){
         basketX = 300;
     }
+
     basket.style.left = basketX + "px";
 });
 
@@ -154,13 +226,46 @@ pauseBtn.addEventListener("click", function(){
     if(!paused){
 
         clearInterval(gameInterval);
+
+        clearInterval(timerInterval);
+
         pauseBtn.innerText = "Resume";
+
         paused = true;
 
     }else{
 
         gameInterval = setInterval(updateBall, 20);
+
+        timerInterval = setInterval(function(){
+
+            timer++;
+
+            timerText.innerText = timer;
+
+        }, 1000);
+
         pauseBtn.innerText = "Pause";
+
         paused = false;
     }
+});
+
+document.addEventListener("touchmove", function(event){
+
+    const gameArea = document.getElementById("gameArea");
+
+    const rect = gameArea.getBoundingClientRect();
+
+    basketX = event.touches[0].clientX - rect.left - 50;
+
+    if(basketX < 0){
+        basketX = 0;
+    }
+
+    if(basketX > 300){
+        basketX = 300;
+    }
+
+    basket.style.left = basketX + "px";
 });
