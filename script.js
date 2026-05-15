69a6bd988333c6bd51fa9e3ae89bf02aeaf76fd5
Kaustavoffx <kaustav.chowdhury9999@gmail.com>
2026-05-15 09:55:49 +0530

    added javascript codes

---
+++ script.js
@@ -0,0 +1,53 @@
+const basket = document.getElementById("basket");
+const ball = document.getElementById("ball")
+const scoreText = document.getElementById("score");
+
+let basketX=150;
+let ballX = Math.random() = 370;
+let ballY = 0;
+
+let score = 0;
+
+document.addEventListener("mousemove", moveBasket);
+
+function moveBasket(event){
+    const gameArea = document.getElementById("gameArea");
+    const rect = gameArea.getBoundingClientRect();
+
+    basketX = event.clientX - rect.left - 50;
+
+    if (basketX <0){
+        basketX =0 ;
+    }
+
+    if (basketX>300){
+        basketX = 300;
+    }
+
+    basket.style.left = basketX + 'px';
+
+}
+
+function updateBall(){
+    ballY += 4;
+
+    ball.style.top = ballY +'px';
+    ball.style.left = ballY +'px';
+
+if(ballY > 550){
+    if(ballX > basketX && ballX < basketX){
+        score++;
+        scoreText.innerText = score;
+
+        }
+
+    resetBall()
+    }
+}
+
+function resetball(){
+    ballY = 0;
+    ballX = Math.random() *370;
+}
+
+setInterval(updateBall, 20);
