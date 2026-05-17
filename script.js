3741ba7c8bcd12645d33d3b0905b24949ee87fbd
Kaustavoffx <kaustav.chowdhury9999@gmail.com>
2026-05-17 13:41:41 +0530

    loading screen js and html added

--- script.js
+++ script.js
@@ -609,3 +609,20 @@ savePlayerBtn.addEventListener(
             playerName.value;
     }
 );
+
+window.addEventListener(
+
+    "load",
+
+    function(){
+
+        setTimeout(function(){
+
+            document.getElementById(
+                "loadingScreen"
+                
+            ).style.display = "none";
+
+        },2000);
+    }
+);
